ee0907d408e366a059e7a0f1c318139022763da3
Aistaaa <estapurwanti051103@gmail.com>
2024-04-08 10:23:45 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'hello world', // Judul aplikasi
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(
+            'Welcome to Flutter by Esta', // Judul header
+            style: TextStyle(
+              color: Colors.white, // Warna teks header
+            ),
+          ),
+          backgroundColor: Color(0xFF81D4FA), // Warna latar belakang header
+        ),
+        body: Center(
+          child: Text(
+            '"Let\'s explore endless possibilities with Flutter!"', // Teks body
+            style: TextStyle(
+              color: Color(0xFF546E7A), // Warna teks body
+              fontSize: 24.0, // Ukuran font teks body
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Nama: Esta Purwanti
+// NIM: STI202102181
+// Tugas: Jobsheet 4 Mobile Programming
