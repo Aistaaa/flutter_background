fd84c2d068ffd92284b3974109ceb4fb4c56210d
Aistaaa <estapurwanti051103@gmail.com>
2024-04-08 12:47:46 +0700

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Hello World', // Judul aplikasi
+      title: 'flutter_background', // Judul aplikasi
       home: Scaffold(
         appBar: AppBar(
           title: Text(
@@ -15,14 +15,19 @@ class MyApp extends StatelessWidget {
               color: Colors.white, // Warna teks header
             ),
           ),
-          backgroundColor: Color(0xFF81D4FA), // Warna latar belakang header
+          backgroundColor: Color.fromARGB(255, 19, 122, 170), // Warna latar belakang header
         ),
-        body: Center(
-          child: Text(
-            '"Let\'s explore endless possibilities with Flutter!"', // Teks body
-            style: TextStyle(
-              color: Color(0xFF546E7A), // Warna teks body
-              fontSize: 24.0, // Ukuran font teks body
+        body: Container(
+          decoration: BoxDecoration(
+            color: Color.fromARGB(255, 202, 214, 218), // Warna latar belakang body
+          ),
+          child: Center(
+            child: Text(
+              '"Let\'s explore endless possibilities with Flutter!"', // Teks body
+              style: TextStyle(
+                color: Color.fromARGB(255, 19, 122, 170), // Warna teks body
+                fontSize: 24.0, // Ukuran font teks body
+              ),
             ),
           ),
         ),
